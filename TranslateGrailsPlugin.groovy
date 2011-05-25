5589c20bddb87f3524fbee08bdae6c1dd5bfe547
Jonathan Griggs <jonathan.griggs@gmail.com>
2011-05-25 00:02:11 -0600

    Changed naming from grails-translate to translate

--- TranslateGrailsPlugin.groovy
+++ TranslateGrailsPlugin.groovy
@@ -1,4 +1,4 @@
-class GrailsTranslateGrailsPlugin {
+class TranslateGrailsPlugin {
     // the plugin version
     def version = "0.1"
     // the version or versions of Grails the plugin is designed for
@@ -19,7 +19,7 @@ Translates text from one language to another, using Google Translate
 '''
 
     // URL to the plugin's documentation
-    def documentation = "http://grails.org/plugin/grails-translate"
+    def documentation = "http://grails.org/plugin/translate"
 
     def doWithWebDescriptor = { xml ->
         // TODO Implement additions to web.xml (optional), this event occurs before 
