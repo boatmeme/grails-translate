5d05393a4d14a7e62e3037f86cacf2e3a7255a52
Jonathan Griggs <jonathan.griggs@gmail.com>
2011-05-26 16:28:44 -0600

    Closes #6 Closes #5 Closes #4

--- grails-app/controllers/com/memetix/translate/TranslateController.groovy
+++ grails-app/controllers/com/memetix/translate/TranslateController.groovy
@@ -1,4 +1,9 @@
 package com.memetix.translate
+
+import grails.converters.JSON
+import grails.converters.XML
+import groovy.xml.MarkupBuilder
+
 /**
  * TranslateController provides an example / test implementation of the Translate plugin
  * 
@@ -38,5 +43,57 @@ class TranslateController {
             flash.error "Language Detection Error: ${e.message}"
         }
         render(view:'index',model:[language:detected])
+    }
+    
+    def ajax = {
+        def translationResult = params?.originalText
+        def startTime = System.currentTimeMillis()
+        def responseObj
+        
+        def format = params?.format?.toLowerCase() ?: 'json'
+        
+        if(!translationResult) {
+            responseObj = ["status_code":"500","status_text":"MISSING_PARAMETER","errors":["Please provide an 'originalText' parameter with the text to be translated"],"data":[]]
+        } else {
+            def expandedMap
+            responseObj = ["status_code":"200","status_text":"OK","data":[],"errors":[]]
+            try {
+                translationResult = translateService.translate(params?.originalText,params?.toLang)
+                responseObj."data" = ["translation":translationResult]
+            } catch(Exception e) {
+                log.error e
+                responseObj."status_code" = "500"
+                responseObj."status_text" = "INVALID_LANGUAGE_PAIR"
+                responseObj."errors".add(e?.getMessage())
+            }
+        }
+        
+        if(format=='json') {
+            responseObj."elapsedTime" = System.currentTimeMillis()-startTime
+            render responseObj as JSON
+        } else if(format=='xml'){
+            def b = new groovy.xml.StreamingMarkupBuilder()
+            b.encoding = "UTF-8"
+            def xml = b.bind {
+                    mkp.xmlDeclaration()
+                    response {
+                        status_code(responseObj."status_code")
+                        status_text(responseObj."status_text")
+                        errors {
+                            for(e in responseObj."errors") {
+                                error(e)
+                            }
+                        }
+                        data {
+                            if(responseObj?.data?.translation)
+                                translation(responseObj."data"."translation")
+                        }
+                        elapsedTime(System.currentTimeMillis()-startTime)
+                    }
+            }
+            render(text: xml.toString(),contentType:'text/xml') 
+        } else {
+            render responseObj."data".translation
+        }
     }
 }
--- test/unit/com/memetix/translate/TranslateControllerTests.groovy
+++ test/unit/com/memetix/translate/TranslateControllerTests.groovy
@@ -1,17 +1,196 @@
 package com.memetix.translate
 
 import grails.test.*
+import org.apache.log4j.*
+import grails.converters.JSON
+import grails.converters.XML
 
 class TranslateControllerTests extends ControllerUnitTestCase {
+    def log
+    def translateService
+    
+    def englishPhrase = "One day last year, while working on a biography of the publisher Scofield Thayer, I opened a folder of papers related to his magazine The Dial. The folder contained undated letters from the poet E.E. Cummings to Thayer";
+    def frenchPhrase = "Un jour l'année dernière, tout en travaillant sur une biographie de l'éditeur Scofield Thayer, j'ai ouvert un dossier de documents liés à sa revue The Dial. Le dossier contenait des lettres non datée du poète EE Cummings à Thayer"
+    
+    def statusCodeOk = "200"
+    def statusCodeServerError = "500"
+    
+    def statusTextOk = "OK"
+    def statusTextMissingParameter = "MISSING_PARAMETER"
+    def statusTextInvalidPair = "INVALID_LANGUAGE_PAIR"
+
+    def jsonFormat = "json"
+    def xmlFormat = "xml"
+    
+    def missingParameterError = "Please provide an 'originalText' parameter with the text to be translated"
+    def translateError = "Google returned the following error: [400] could not reliably detect source language"
     protected void setUp() {
         super.setUp()
+        setupLogger()
+        translateService = new TranslateService()
+        controller = new TranslateController()
+        controller.translateService = translateService
+        controller.response.setCharacterEncoding("UTF-8") 
+    }
+    
+    private setupLogger() {
+        // build a logger...
+        BasicConfigurator.configure() 
+        LogManager.rootLogger.level = Level.DEBUG
+        log = LogManager.getLogger("TranslateService")
+
+        // use groovy metaClass to put the log into your class
+        TranslateService.class.metaClass.getLog << {-> log}
     }
 
     protected void tearDown() {
         super.tearDown()
     }
+    
+    private assertCommonResponseParams(response) {
+        assertNotNull   response
+        assertNotNull   response.status_code
+        assertNotNull   response.status_text
+        assertNotNull   response.data
+        assertNotNull   response.errors
+        assertNotNull   response.elapsedTime
+    }
+    
+    private parseXMLToMap(xmlResponse) {
+        def map = new HashMap()
+        for(int i=0;i<xmlResponse?.data?.entry?.size();i++) {
+            def xmlObj = xmlResponse?.data?.entry[i];
+            map.put(xmlObj.translation.toString(), xmlObj) 
+        }
+        return map
+    }
 
-    void testSomething() {
+    void testIndex() {
+        def model = controller.index()
+        assertNull   model
+    }
+    
+    void testTranslate() {
+        controller.params.originalText = englishPhrase
+        controller.params.toLang = "fr"
+        def model = controller.translate()
+        assertNotNull   model
+        assertNotNull   model.translation
+        assertNull      controller.flash.error
+        assertEquals    frenchPhrase,model.translation
+    }
+    
+    void testDetect() {
+        controller.params.originalText = englishPhrase
+        def model = controller.detect()
+        assertNotNull   model
+        assertNotNull   model.language
+        assertNull      controller.flash.error
+        assertEquals    "en",model.language
+    }
+    
+    void testTranslate_InvalidLanguagePair() {
+        controller.params.originalText = englishPhrase
+        controller.params.toLang = "hy"
+        def model = controller.translate()
+        assertNotNull   model
+        assertNotNull   model.translation
+        assertNotNull   controller.flash.error
+        assertEquals    englishPhrase,model.translation
+    }
 
+    void testAjax_JSON_Translate() {
+        controller.params.originalText = englishPhrase
+        controller.params.toLang = "fr"
+        controller.ajax()    
+        
+        def jsonResponse = JSON.parse(controller.response.contentAsString)
+        assertCommonResponseParams(jsonResponse)
+        
+        assertEquals statusCodeOk,  jsonResponse.status_code.toString()
+        assertEquals statusTextOk,  jsonResponse.status_text.toString()
+        assertEquals 1,             jsonResponse.data.size()
+        
+        assertEquals frenchPhrase,    jsonResponse?.data?.translation     ?.toString()
+    }
+    
+    void testAjax_JSON_InvalidLanguagePair() {
+        controller.params.originalText = englishPhrase
+        controller.params.toLang = "hy"
+        controller.ajax()    
+        
+        def response = JSON.parse(controller.response.contentAsString)
+        assertCommonResponseParams(response)
+        
+        assertEquals statusCodeServerError,     response    ?.status_code   ?.toString()
+        assertEquals statusTextInvalidPair,     response    ?.status_text   ?.toString()
+        
+        assertEquals 1,                         response    ?.errors     ?.size()
+        assertEquals translateError,     response  ?.errors[0]  ?.toString()
+        assertEquals 0,                         response    ?.data.entry        ?.size()
+    }
+    
+     void testAjax_JSON_MissingParameter() {
+        controller.params.toLang = "hy"
+        controller.ajax()    
+        
+        def response = JSON.parse(controller.response.contentAsString)
+        assertCommonResponseParams(response)
+        
+        assertEquals statusCodeServerError,     response    ?.status_code   ?.toString()
+        assertEquals statusTextMissingParameter,response    ?.status_text   ?.toString()
+        
+        assertEquals 1,                         response    ?.errors      ?.size()
+        assertEquals missingParameterError,     response    ?.errors[0]  ?.toString()
+        assertEquals 0,                         response    ?.data.entry        ?.size()
+    }
+    
+    void testAjax_XML_Translate() {
+        controller.params.originalText = englishPhrase
+        controller.params.toLang = "fr"
+        controller.params.format = xmlFormat
+        controller.ajax()    
+        
+        def response = XML.parse(controller.response.contentAsString)
+        assertCommonResponseParams(response)
+        
+        assertEquals statusCodeOk,  response.status_code.toString()
+        assertEquals statusTextOk,  response.status_text.toString()
+        assertEquals 1,             response.data.size()
+         
+        assertEquals frenchPhrase,  response?.data?.translation     ?.toString()
+    }
+    
+    void testAjax_XML_InvalidLanguagePair() {
+        controller.params.originalText = englishPhrase
+        controller.params.toLang = "hy"
+        controller.params.format = xmlFormat
+        controller.ajax()    
+        
+        def response = XML.parse(controller.response.contentAsString)
+        assertCommonResponseParams(response)
+        
+        assertEquals statusCodeServerError,     response    ?.status_code   ?.toString()
+        assertEquals statusTextInvalidPair,     response    ?.status_text   ?.toString()
+        
+        assertEquals 1,                         response    ?.errors     ?.size()
+        assertEquals translateError,            response    ?.errors[0]  ?.toString()
+        assertEquals 0,                         response    ?.data.entry        ?.size()
+    }
+    
+     void testAjax_XML_MissingParameter() {
+        controller.params.toLang = "hy"
+        controller.params.format = xmlFormat
+        controller.ajax()
+
+        def response = XML.parse(controller.response.contentAsString)
+        assertCommonResponseParams(response)
+        
+        assertEquals statusCodeServerError,     response    ?.status_code   ?.toString()
+        assertEquals statusTextMissingParameter,response    ?.status_text   ?.toString()
+        
+        assertEquals 1,                         response    ?.errors      ?.size()
+        assertEquals missingParameterError,     response    ?.errors[0]  ?.toString()
+        assertEquals 0,                         response    ?.data.entry        ?.size()
     }
 }
