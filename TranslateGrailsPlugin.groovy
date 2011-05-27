547f08eac528ac3bb7fc298fc8af9a3838998440
Jonathan Griggs <jonathan.griggs@gmail.com>
2011-05-26 23:10:37 -0600

    Set minVersion = Grails 1.1

--- TranslateGrailsPlugin.groovy
+++ TranslateGrailsPlugin.groovy
@@ -2,7 +2,7 @@ class TranslateGrailsPlugin {
     // the plugin version
     def version = "1.0"
     // the version or versions of Grails the plugin is designed for
-    def grailsVersion = "1.3.7 > *"
+    def grailsVersion = "1.1 > *"
     // the other plugins this plugin depends on
     def dependsOn = [:]
     // resources that are excluded from plugin packaging
